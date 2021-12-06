a7dc735d05487eeb86e0ef8f0efc021ea9502c8a
nameerfarooq <nameerfarooquit@gmail.com>
2021-12-06 22:09:16 +0500

    3

--- q2.scala
+++ q2.scala
@@ -1,5 +1,8 @@
 object Main{
     def main(args:Array[String]): Unit = {
+
+        // maps
+
         var A:Map[Char, Int] = Map()
         val colors = scala.collection.mutable.Map("red"->"#FF0000","azure"->"#F0FFFF")
         colors("red") = "#xyz"
@@ -12,6 +15,13 @@ object Main{
             println(i)
         }
 
+        //sets
+        val set1=Set(1,2,3)
+        // set1+= (5) this will give error as it is an immutable set
+        println(set1)
+        var set2=Set(2,8,9)
+        set2+=(6)
+        println(set2)
 
        
     }
